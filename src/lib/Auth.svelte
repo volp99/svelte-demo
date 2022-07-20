415b70e6f89d5626f3e72b57ebafaa72b85b894c
volp99 <stefanovolpatti@yahoo.it>
2022-07-20 15:54:40 +0200

    Install prettier and add example auth

---
+++ src/lib/Auth.svelte
@@ -0,0 +1,29 @@
+<script lang="ts">
+	let user = {loggedIn: false};
+
+	function handleToggle() {
+		user.loggedIn = !user.loggedIn;
+	}
+</script>
+
+{#if user.loggedIn}
+    <button on:click={handleToggle}>Logout</button>
+{:else}
+    <button on:click={handleToggle}>Login</button>
+{/if}
+
+<style>
+	button {
+		background: #ff3e00;
+		color: white;
+		padding: 10px;
+		border: none;
+		border-radius: 5px;
+		cursor: pointer;
+		width: 100px;
+	}
+
+	button:hover {
+		background: #e8683f;
+	}
+</style>
